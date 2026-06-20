81d470d0d14b0d6958bad2c46ef1d1d0d71ea1c6
Victoria <dr.v.orlova@gmail.com>
2026-06-20 17:36:24 +0300

    feat: update eslint packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,43 +1,43 @@
-import antfu from "@antfu/eslint-config";
+import antfu from '@antfu/eslint-config'
 
 export default antfu(
   {
-    type: "app",
+    type: 'app',
     react: true,
     typescript: true,
     formatters: true,
     jsonc: false,
     yaml: false,
     stylistic: {
-      arrowParens: "avoid",
+      arrowParens: 'avoid',
       bracketSpacing: true,
       bracketSameLine: false,
       printWidth: 100,
-      proseWrap: "preserve",
-      quoteProps: "as-needed",
+      proseWrap: 'preserve',
+      quoteProps: 'as-needed',
       semi: false,
       singleQuote: true,
       tabWidth: 2,
-      trailingComma: "es5",
+      trailingComma: 'es5',
       useTabs: false,
     },
   },
   {
     rules: {
-      "ts/no-redeclare": "off",
-      "ts/consistent-type-definitions": ["error", "type"],
-      "no-console": ["warn"],
-      "antfu/no-top-level-await": ["off"],
-      "node/prefer-global/process": ["off"],
-      "node/no-process-env": ["error"],
-      "perfectionist/sort-imports": [
-        "error",
+      'ts/no-redeclare': 'off',
+      'ts/consistent-type-definitions': ['error', 'type'],
+      'no-console': ['warn'],
+      'antfu/no-top-level-await': ['off'],
+      'node/prefer-global/process': ['off'],
+      'node/no-process-env': ['error'],
+      'perfectionist/sort-imports': [
+        'error',
         {
           tsconfig: {
-            rootDir: ".",
+            rootDir: '.',
           },
         },
       ],
     },
-  }
-);
+  },
+)
