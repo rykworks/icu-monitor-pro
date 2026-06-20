54dda592bc276d040afd54f8ec61a52e6943e7c3
Victoria <dr.v.orlova@gmail.com>
2026-06-20 17:31:03 +0300

    init: initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import antfu from "@antfu/eslint-config";
+
+export default antfu(
+  {
+    type: "app",
+    react: true,
+    typescript: true,
+    formatters: true,
+    jsonc: false,
+    yaml: false,
+    stylistic: {
+      arrowParens: "avoid",
+      bracketSpacing: true,
+      bracketSameLine: false,
+      printWidth: 100,
+      proseWrap: "preserve",
+      quoteProps: "as-needed",
+      semi: false,
+      singleQuote: true,
+      tabWidth: 2,
+      trailingComma: "es5",
+      useTabs: false,
+    },
+  },
+  {
+    rules: {
+      "ts/no-redeclare": "off",
+      "ts/consistent-type-definitions": ["error", "type"],
+      "no-console": ["warn"],
+      "antfu/no-top-level-await": ["off"],
+      "node/prefer-global/process": ["off"],
+      "node/no-process-env": ["error"],
+      "perfectionist/sort-imports": [
+        "error",
+        {
+          tsconfig: {
+            rootDir: ".",
+          },
+        },
+      ],
+    },
+  }
+);
