1303ebf12d7609b2b3219c050716db9e710684d5
Victoria <dr.v.orlova@gmail.com>
2026-06-20 18:55:43 +0300

    feat: add initial modular setup and react router

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, 'src/app'),
+      '@modules': path.resolve(__dirname, 'src/modules'),
+      '@shared': path.resolve(__dirname, 'src/shared'),
+    },
+  },
 })
