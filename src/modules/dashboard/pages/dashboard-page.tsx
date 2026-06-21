4d4eb92ff8349d40be598bc808ba2d6f6af47f7b
Viktoria Rykovskaya <dr.v.orlova@gmail.com>
2026-06-21 18:41:51 +0300

    feat: add design tokens and basic ui kit (#4)
    * feat: add base ui components
    
    * feat: update design tokens
    
    * fix: fix type issues at shadcn components

--- src/modules/dashboard/pages/dashboard-page.tsx
+++ src/modules/dashboard/pages/dashboard-page.tsx
@@ -1,3 +1,11 @@
+import { Badge, Button } from '@/shared/ui'
+
 export default function DashboardPage() {
-  return <div className="text-primary">DashboardPage</div>
+  return (
+    <div className="text-primary">
+      DashboardPage
+      <Button>Click me</Button>
+      <Badge variant="ghost">Badge me</Badge>
+    </div>
+  )
 }
